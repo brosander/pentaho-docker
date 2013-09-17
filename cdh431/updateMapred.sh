04a26ac0aaeec718b4a889f737063acfa6d64f76
bryan <bryan@docker.(none)>
2013-09-17 17:11:33 -0400

    Hardcoding jobtracker port

--- cdh431/updateMapred.sh
+++ cdh431/updateMapred.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python /root/hadoopProperties.py -i -f /etc/hadoop/conf/mapred-site.xml -p mapred.job.tracker -v hdfs://$JOBTRACKER:$JOBTRACKER_PORT/
+python /root/hadoopProperties.py -i -f /etc/hadoop/conf/mapred-site.xml -p mapred.job.tracker -v hdfs://$JOBTRACKER:8021/
